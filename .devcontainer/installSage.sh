6d9d6bb0860cf98690582c9653c0b9deba00e7c1
Oscar Levin <oscar.levin@gmail.com>
2025-04-07 23:50:41 +0000

    add project

--- .devcontainer/installSage.sh
+++ .devcontainer/installSage.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# This file was automatically generated with PreTeXt 2.15.2.
+# This file was automatically generated with PreTeXt 2.16.0.
 # If you modify this file, PreTeXt will no longer automatically update it.
 
 # Conda should already be installed in the codespace.  We need to add the conda-forge channel
